4c58ffd2f297330b1d081ea4a7bd6c7a7ef29b27
Jason Edstrom <jason@instacode.io>
2020-01-30 09:51:08 -0800

    initial service class commit

---
+++ Magenic Employee/Employee/EmployeeService.swift
@@ -0,0 +1,28 @@
+//
+// Created by Jason Edstrom on 1/30/20.
+// Copyright (c) 2020 Jason Edstrom. All rights reserved.
+//
+
+import Foundation
+
+class EmployeeService {
+
+    func getEmployees() {
+
+    }
+
+    func getEmployee(id:Int)
+    {
+
+    }
+
+    func deleteEmployee(id:Int)
+    {
+        
+    }
+    
+    func buildRequest() {
+        
+    }
+
+}
